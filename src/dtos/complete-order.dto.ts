b1170e3d822342dfe3e22445957d0a297d92b6d0
Jose Alejandro <jose.arrauttg@pragma.com.co>
2024-06-19 01:08:31 -0500

    feat: Use swagger tags for dtos documentation

--- src/dtos/complete-order.dto.ts
+++ src/dtos/complete-order.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CompleteOrderDto {
+  @ApiProperty({ description: 'The ID of the order' })
   @IsNotEmpty()
   @IsString()
   readonly orderId: string;
 
+  @ApiProperty({ description: 'The ID of the recipe' })
   @IsNotEmpty()
   @IsString()
   readonly recipeId: string;
 
+  @ApiProperty({ description: 'The name of the recipe' })
   @IsNotEmpty()
   @IsString()
   readonly recipeName: string;
--- src/dtos/pagination-query.dto.ts
+++ src/dtos/pagination-query.dto.ts
@@ -3,13 +3,13 @@ import { Type } from 'class-transformer';
 import { IsInt, Min } from 'class-validator';
 
 export class PaginationQueryDto {
-  @ApiProperty({ description: 'Page number', minimum: 1 })
+  @ApiProperty({ description: 'Page number', example: 1 })
   @Type(() => Number)
   @IsInt()
   @Min(1)
   page: number;
 
-  @ApiProperty({ description: 'Number of items per page', minimum: 1 })
+  @ApiProperty({ description: 'Number of items per page', example: 10 })
   @Type(() => Number)
   @IsInt()
   @Min(1)
